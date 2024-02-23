354f0576a6613fb5299e175c9eda0b343b4fc738
reroro1 <153210591+reroro1@users.noreply.github.com>
2024-02-23 15:14:45 +0900

    rsp.c

--- rsp.c
+++ rsp.c
@@ -32,3 +32,48 @@ int main() {
 
     return 0;
 }
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h> // 랜덤 함수 사용하기 위해
+#include <time.h> // 시간 설정
+
+int Gresult(int u, int com) {
+    int win[3][3] = {
+        {0, -1, 1},  // 비김, 짐, 이김 주먹 경우의 수 00 01 02
+        {1, 0, -1},  // 이김, 비김, 짐 보자기 경우의 수 10 11 12
+        {-1, 1, 0}   // 짐, 이김, 비김 가위 경우의 수 20 21 22
+    };
+    return win[u][com];
+}
+int main() {
+    int u = 0;
+    while (1) {
+        srand(time(NULL));
+        int com = rand() % 3;  // 0 ~ 2까지 난수 선택 
+        printf("0 바위, 1 보자기, 2 가위 : ");
+        scanf("%d", &u);
+        printf("com = %d, u = %d\n", com, u);
+
+        int result = Gresult(u, com); // 함수 호출
+
+        switch (result) {
+        case 0:
+            printf("draw\n");
+            break;
+        case 1:
+            printf("win.\n");
+            break;
+        case -1:
+            printf("lose.\n");
+            break;
+        default:
+            printf("잘못 입력\n");
+            break;
+        }
+    }
+
+    return 0;
+}
+
